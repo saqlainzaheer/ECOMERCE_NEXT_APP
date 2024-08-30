c9fb6df435bc19d4921f8bace1411ddea7426eef
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-08-30 22:33:32 +0500

    prodcut remove

--- app/page.js
+++ app/page.js
@@ -20,15 +20,7 @@ const topSellingsData = [
     discountPrice: null,
     discount: null,
   },
-  
-  {
-    image: "/image1.png",
-    title: "T-SHIRT WITH TAPE DETAILS",
-    rating: 3,
-    realPrice: "$160",
-    discountPrice: null,
-    discount: null,
-  },
+
   
   {
     image: "/image1.png",
