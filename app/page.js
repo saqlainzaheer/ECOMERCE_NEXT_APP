98104834d99c88b364c7a9722d8a6d2cd9f073f6
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-08-30 22:33:46 +0500

    product remove

--- app/page.js
+++ app/page.js
@@ -22,15 +22,7 @@ const topSellingsData = [
   },
 
   
-  {
-    image: "/image1.png",
-    title: "T-SHIRT WITH TAPE DETAILS",
-    rating: 3,
-    realPrice: "$160",
-    discountPrice: null,
-    discount: null,
-  },
-  {
+
     image: "/image2.png",
     title: "SKINNY FIT JEANS",
     rating: 4.5,
