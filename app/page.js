b35972e796851e55cca9d419e874fc31f40d966b
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-09-01 17:29:22 +0500

    bug007 fix

--- app/page.js
+++ app/page.js
@@ -29,6 +29,14 @@ const topSellingsData = [
     discountPrice: null,
     discount: null,
   },
+  {
+    image: "/image1.png",
+    title: "T-SHIRT WITH TAPE DETAILS",
+    rating: 3,
+    realPrice: "$160",
+    discountPrice: null,
+    discount: null,
+  },
 
   
 
