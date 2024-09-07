92497b4f67492d5e4910ec67f61df2931f6a0f9c
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-09-07 22:39:33 +0500

    fix som data

--- app/page.js
+++ app/page.js
@@ -32,14 +32,7 @@ const topSellingsData = [
   
  
  
-  {
-    image: "/image1.png",
-    title: "T-SHIRT WITH TAPE DETAILS",
-    rating: 3,
-    realPrice: "$160",
-    discountPrice: null,
-    discount: null,
-  },
+  
   {
     image: "/image1.png",
     title: "T-SHIRT WITH TAPE DETAILS",
