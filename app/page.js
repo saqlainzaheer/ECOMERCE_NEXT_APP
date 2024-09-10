ddd046760cfc37ea7226fe54c1c3d8004bc87615
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-09-10 21:55:22 +0500

    add product

--- app/page.js
+++ app/page.js
@@ -13,15 +13,6 @@ const topSellingsData = [
     discountPrice: null,
     discount: null,
   },
-  {
-    image: "/image1.png",
-    title: "T-SHIRT WITH TAPE DETAILS",
-    rating: 3,
-    realPrice: "$160",
-    discountPrice: null,
-    discount: null,
-  },
- 
  
  
 
