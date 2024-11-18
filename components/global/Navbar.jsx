e1349a9291203f76d2e05155619bfc50b20a2b83
Saqlain Code Stack <saqlainwebstudio@gmail.com>
2024-11-18 21:44:45 +0500

    link removed

--- components/global/Navbar.jsx
+++ components/global/Navbar.jsx
@@ -38,9 +38,7 @@ export default function Navbar() {
           <Link href="/login" className="hover:text-blue-500 border-2 hover:border-blue-500 border-white text-white focus:outline-none font-medium text-md rounded-md text-sm py-1.5 px-4 mx-2 transition-all">
             Sign In
           </Link>
-          <Link href="/login" className="hover:text-blue-500 border-2 hover:border-blue-500 border-white text-white focus:outline-none font-medium text-md rounded-md text-sm py-1.5 px-4 mx-2 transition-all">
-            Sign In
-          </Link>
+          
           <button
             onClick={toggleMenu}
             className="togglenavbtn inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
